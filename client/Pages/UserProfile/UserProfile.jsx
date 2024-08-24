b111df47a8852500a1af0fcb7630063f275e43cb
lynnjaco <jacobs.lynnette@gmail.com>
2024-08-23 21:45:34 -0400

    feat:Add image_url to showUserEvents query
    - Modified showUserEvents function in users.js
    - Included image_url in the SELECT statement of the SQL query
    - This change enhances the event data returned, providing frontend with event images

--- client/Pages/UserProfile/UserProfile.jsx
+++ client/Pages/UserProfile/UserProfile.jsx
@@ -21,10 +21,37 @@ function UserProfile() {
        .then(data => setUsersFriends(data))
     }, [id]);  
 
+    useEffect(() => {
+        fetch(`${API}/users/${id}/events`)
+       .then(res => res.json())
+       .then(data => setUsersEvents(data))
+    }, [id]);
+
     return (
         <>
            <div>
             <h2>User's Events</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                {usersEvents.map(event => (
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="rounded-t-lg" src={ event.image_url } alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ event.event_name }</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{ event.registration_date }.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                More Details
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
 
            </div>
             <h2>User's Friends</h2>
